37f7eaee8c24d60f14b03e1b6ea7dfe0c4124664
P5LNP <39556589+P5LNP@users.noreply.github.com>
2018-05-24 14:57:48 +0100

    Scripts from Course

---
+++ Check User Account Expiry.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host "TDX Account Lockout Check"
+
+$username = read-host "Enter Username"
+
+get-aduser -filter {samaccountname -like $sam} -properties * | select-object DisplayName,SamAccountName,PasswordExpired,PasswordLastSet,@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}},Enabled,LockedOut | Format-List
---
+++ Create VM.ps1
@@ -0,0 +1,30 @@
+﻿Write-Host "TDX Hyper-V Virtual Machine Creation Script"
+
+"="*50
+
+#Parameters
+
+$VMName = Read-Host "Give Me a VM Name"
+$RAM = 4GB
+$CPUCores = Read-Host "Enter Number of Cores"
+$OSVHDSize = 5GB
+$DataVHDSize = 5GB
+$VHDLocation = "C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks"
+$VMLocation = "C:\ProgramData\Microsoft\Windows\Hyper-V"
+$VlanID = Read-Host "Enter Vlan ID"
+
+#Copy Template VHD
+
+#Start-BitsTransfer -Source C:\clientsourcedir\*.txt `-Destination c:\clientdir\ -TransferType Download
+
+
+
+#BuildVM
+
+New-VM -Name $VMName -MemoryStartupBytes $RAM -NewVHDPath $VHDLocation\$VMName`_System.vhdx -Path $VMLocation -NewVHDSizeBytes $OSVHDSize -Generation 2 -Switch "Private Network"
+Set-VM -Name $VMName -ProcessorCount $CPUCores -StaticMemory:$true
+Set-VMNetworkAdapterVlan -VMName $VMName -Access -VlanId $VlanID
+New-VHD -Path "$VHDLocation\$VMName`_Data.vhdx" -Fixed -SizeBytes $DataVHDSize
+Add-VMHardDiskDrive -VMName $VMName -Path "$VHDLocation\$VMName`_Data.vhdx"
+
+Pause
