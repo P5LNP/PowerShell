b37bbe38da7545dfd793226c181d0df06c0a5973
P5LNP <39556589+P5LNP@users.noreply.github.com>
2018-05-25 10:24:52 +0100

    Updated Username Variable

--- Check User Account Expiry.ps1
+++ Check User Account Expiry.ps1
@@ -2,4 +2,4 @@
 
 $username = read-host "Enter Username"
 
-get-aduser -filter {samaccountname -like $sam} -properties * | select-object DisplayName,SamAccountName,PasswordExpired,PasswordLastSet,@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}},Enabled,LockedOut | Format-List
+get-aduser -filter {samaccountname -like $username} -properties * | select-object DisplayName,SamAccountName,PasswordExpired,PasswordLastSet,@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}},Enabled,LockedOut | Format-List
